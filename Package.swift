901b823e5661c66e08cb5052e3d208d8f5a6b241
Erid <erid.bardhaj@grandcentrix.net>
2021-03-29 12:26:10 +0200

    Add GithubPackageModule to SwiftPackage

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "GithubSwiftPackage",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GithubSwiftPackage",
-            targets: ["GithubSwiftPackage"]),
+            targets: ["GithubSwiftPackage", "GithubPackageModule"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,8 +22,10 @@ let package = Package(
         .target(
             name: "GithubSwiftPackage",
             dependencies: []),
-        .testTarget(
-            name: "GithubSwiftPackageTests",
-            dependencies: ["GithubSwiftPackage"]),
+        .binaryTarget(
+            name: "GithubPackageModule",
+            url: "https://maven.pkg.github.com/eridbardhaj/GithubPackageModule/githubpackagemodule/0.0.1/GithubPackageModule.xcframework.zip",
+            checksum: ""
+        )
     ]
 )
